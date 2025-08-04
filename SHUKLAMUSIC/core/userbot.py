3eb3efc6161821692a6db4f1ef4bdef71ba0fafb
Ahad6626 <asad575530@gmail.com>
2025-08-04 19:45:37 +0530

    Update userbot.py

--- SHUKLAMUSIC/core/userbot.py
+++ SHUKLAMUSIC/core/userbot.py
@@ -1,21 +1,192 @@
+import sys
+
 from pyrogram import Client
-import re
-from os import getenv
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from dotenv import load_dotenv
-from pyrogram import filters
-load_dotenv()
+
 import config
-from dotenv import load_dotenv
+
 from ..logging import LOGGER
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-STRING_SESSION = getenv("STRING_SESSION", "")
-TEST_ID = int("-1002759072638")
+
+assistants = []
+assistantids = []
 
 
-    
+class Userbot(Client):
+    def __init__(self):
+        self.one = Client(
+            name="IvanOne",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING1),
+            no_updates=True,
+        )
+        self.two = Client(
+            name="IvanTwo",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING2),
+            no_updates=True,
+        )
+        self.three = Client(
+            name="IvanThree",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING3),
+            no_updates=True,
+        )
+        self.four = Client(
+            name="IvanFour",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING4),
+            no_updates=True,
+        )
+        self.five = Client(
+            name="IvanFive",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING5),
+            no_updates=True,
+        )
 
-
-
-            
+    async def start(self):
+        LOGGER(__name__).info(f"Starting Assistant Clients...")
+        if config.STRING1:
+            await self.one.start()
+            try:
+                await self.one.join_chat("Ivanxmusicxupdates")
+                await self.one.join_chat("ivan_telegrams")
+                await self.one.join_chat("ivan_telegrams")
+            except Exception:
+                pass
+            assistants.append(1)
+            try:
+                await self.one.send_message(
+                    config.LOG_GROUP_ID,
+                    "-1002759072638",
+                )
+            except Exception:
+                LOGGER(__name__).error(
+                    f"Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
+                )
+                sys.exit()
+            get_me = await self.one.get_me()
+            self.one.username = get_me.username
+            self.one.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.one.name = get_me.first_name + " " + get_me.last_name
+            else:
+                self.one.name = get_me.first_name
+            LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
+        if config.STRING2:
+            await self.two.start()
+            try:
+                await self.two.join_chat("Ivanxmusicxupdates")
+                await self.two.join_chat("ivan_telegrams")
+                await self.two.join_chat("ivan_telegrams")
+            except Exception:
+                pass
+            assistants.append(2)
+            try:
+                await self.two.send_message(
+                    config.LOG_GROUP_ID,
+                    "-1002759072638",
+                )
+            except Exception:
+                LOGGER(__name__).error(
+                    f"Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
+                )
+                sys.exit()
+            get_me = await self.two.get_me()
+            self.two.username = get_me.username
+            self.two.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.two.name = get_me.first_name + " " + get_me.last_name
+            else:
+                self.two.name = get_me.first_name
+            LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
+        if config.STRING3:
+            await self.three.start()
+            try:
+                await self.three.join_chat("Ivanxmusicxupdates")
+                await self.three.join_chat("ivan_telegrams")
+                await self.three.join_chat("ivan_telegrams")
+            except Exception:
+                pass
+            assistants.append(3)
+            try:
+                await self.three.send_message(
+                    config.LOG_GROUP_ID,
+                    "-1002759072638",
+                )
+            except Exception:
+                LOGGER(__name__).error(
+                    f"Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
+                )
+                sys.exit()
+            get_me = await self.three.get_me()
+            self.three.username = get_me.username
+            self.three.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.three.name = get_me.first_name + " " + get_me.last_name
+            else:
+                self.three.name = get_me.first_name
+            LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
+        if config.STRING4:
+            await self.four.start()
+            try:
+                await self.four.join_chat("Ivanxmusicxupdates")
+                await self.four.join_chat("ivan_telegrams")
+                await self.four.join_chat("ivan_telegrams")
+            except Exception:
+                pass
+            assistants.append(4)
+            try:
+                await self.four.send_message(
+                    config.LOG_GROUP_ID,
+                    "-1002759072638",
+                )
+            except Exception:
+                LOGGER(__name__).error(
+                    f"Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
+                )
+                sys.exit()
+            get_me = await self.four.get_me()
+            self.four.username = get_me.username
+            self.four.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.four.name = get_me.first_name + " " + get_me.last_name
+            else:
+                self.four.name = get_me.first_name
+            LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
+        if config.STRING5:
+            await self.five.start()
+            try:
+                await self.five.join_chat("Ivanxmusicxupdates")
+                await self.five.join_chat("ivan_telegrams")
+                await self.five.join_chat("ivan_telegrams")
+            except Exception:
+                pass
+            assistants.append(5)
+            try:
+                await self.five.send_message(
+                    config.LOG_GROUP_ID,
+                    "-1002759072638",
+                )
+            except Exception:
+                LOGGER(__name__).error(
+                    f"Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin ! "
+                )
+                sys.exit()
+            get_me = await self.five.get_me()
+            self.five.username = get_me.username
+            self.five.id = get_me.id
+            assistantids.append(get_me.id)
+            if get_me.last_name:
+                self.five.name = get_me.first_name + " " + get_me.last_name
+            else:
+                self.five.name = get_me.first_name
+            LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
